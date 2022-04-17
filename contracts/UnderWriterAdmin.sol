40c0b4a7f3e60971fb9e52cf2ee5891dad88f388
Simon Zhang <zhanghan.simon@gmail.com>
2022-04-16 23:58:53 -0400

    add admin for newly deployed ERC20Mintable...Upgradable; add isCEther to distinguish CErc20 from CEther

--- contracts/UnderWriterAdmin.sol
+++ contracts/UnderWriterAdmin.sol
@@ -1,290 +1,243 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
-import "./CToken.sol";
-import "./PriceOracle.sol";
-import "./ErrorReporter.sol";
-import "./Governance/Comp.sol";
-import "./UnderwriterStorage.sol";
-import "./UnderwriterProxy.sol";
+import './CToken.sol';
+import './PriceOracle.sol';
+import './ErrorReporter.sol';
+import './Governance/Comp.sol';
+import './UnderwriterStorage.sol';
+import './UnderwriterProxy.sol';
 
-contract UnderwriterAdmin is UnderwriterStorage, ComptrollerErrorReporter {
-    /// @notice Emitted when an action is paused globally
-    event ActionPaused(string action, bool pauseState);
+contract UnderwriterAdmin is UnderwriterStorage, UnderwriterAdminInterface, ComptrollerErrorReporter {
+  /// @notice Emitted when an action is paused globally
+  event ActionPaused(string action, bool pauseState);
 
-    /// @notice Emitted when an action is paused on a market
-    event ActionPaused(CToken cToken, string action, bool pauseState);
+  /// @notice Emitted when an action is paused on a market
+  event ActionPaused(CToken cToken, string action, bool pauseState);
 
-    /// @notice Emitted when borrow cap for a cToken is changed
-    event NewBorrowCap(CToken indexed cToken, uint256 newBorrowCap);
+  /// @notice Emitted when borrow cap for a cToken is changed
+  event NewBorrowCap(CToken indexed cToken, uint256 newBorrowCap);
 
-    /// @notice Emitted when borrow cap guardian is changed
-    event NewBorrowCapGuardian(
-        address oldBorrowCapGuardian,
-        address newBorrowCapGuardian
+  /// @notice Emitted when borrow cap guardian is changed
+  event NewBorrowCapGuardian(address oldBorrowCapGuardian, address newBorrowCapGuardian);
+
+  /// @notice Emitted when pause guardian is changed
+  event NewPauseGuardian(address oldPauseGuardian, address newPauseGuardian);
+
+  constructor(address _gov) public {
+    admin = msg.sender;
+    governanceToken = _gov;
+    suTokenRateMantissa = 10**18;
+  }
+
+  function _become(UnderwriterProxy proxy) public {
+    require(msg.sender == proxy.admin(), 'only unitroller admin can change brains');
+    require(proxy._acceptImplementation() == 0, 'change not authorized');
+  }
+
+  function setEqAssetGroup(
+    uint8 groupId,
+    string memory groupName,
+    uint256 inGroupCTokenRateMantissa,
+    uint256 inGroupSuTokenRateMantissa,
+    uint256 interGroupCTokenRateMantissa,
+    uint256 interGroupSuTokenRateMantissa
+  ) public returns (uint256) {
+    // Check caller is admin
+    if (msg.sender != admin) {
+      return fail(Error.UNAUTHORIZED, FailureInfo.SET_EQUAL_ASSET_GROUP_OWNER_CHECK);
+    }
+
+    eqAssetGroup[groupId] = EqualAssets(
+      groupId,
+      groupName,
+      inGroupCTokenRateMantissa,
+      inGroupSuTokenRateMantissa,
+      interGroupCTokenRateMantissa,
+      interGroupSuTokenRateMantissa
     );
+    equalAssetsGroupNum++;
+    return uint256(Error.NO_ERROR);
+  }
 
-    /// @notice Emitted when pause guardian is changed
-    event NewPauseGuardian(address oldPauseGuardian, address newPauseGuardian);
-
-    constructor(address _gov) public {
-        admin = msg.sender;
-        governanceToken = _gov;
-        suTokenRateMantissa = 10**18;
+  function removeEqAssetGroup(uint8 groupId) public returns (uint256) {
+    // Check caller is admin
+    if (msg.sender != admin) {
+      return fail(Error.UNAUTHORIZED, FailureInfo.SET_EQUAL_ASSET_GROUP_OWNER_CHECK);
     }
 
-    function _become(UnderwriterProxy proxy) public {
-        require(
-            msg.sender == proxy.admin(),
-            "only unitroller admin can change brains"
-        );
-        require(proxy._acceptImplementation() == 0, "change not authorized");
+    delete eqAssetGroup[groupId];
+    equalAssetsGroupNum--;
+    return uint256(Error.NO_ERROR);
+  }
+
+  function getEqAssetGroup(uint8 groupId) public view returns (EqualAssets memory) {
+    return eqAssetGroup[groupId];
+  }
+
+  function getEqAssetGroupNum() public view returns (uint8) {
+    return equalAssetsGroupNum;
+  }
+
+  /**
+   * @notice Admin function to change the Pause Guardian
+   * @param newPauseGuardian The address of the new Pause Guardian
+   * @return uint 0=success, otherwise a failure. (See enum Error for details)
+   */
+  function _setPauseGuardian(address newPauseGuardian) public returns (uint256) {
+    if (msg.sender != admin) {
+      return fail(Error.UNAUTHORIZED, FailureInfo.SET_PAUSE_GUARDIAN_OWNER_CHECK);
     }
 
-    function setEqAssetGroup(
-        uint8 groupId,
-        string memory groupName,
-        uint256 inGroupCTokenRateMantissa,
-        uint256 inGroupSuTokenRateMantissa,
-        uint256 interGroupCTokenRateMantissa,
-        uint256 interGroupSuTokenRateMantissa
-    ) public returns (uint256) {
-        // Check caller is admin
-        if (msg.sender != admin) {
-            return
-                fail(
-                    Error.UNAUTHORIZED,
-                    FailureInfo.SET_EQUAL_ASSET_GROUP_OWNER_CHECK
-                );
-        }
+    // Save current value for inclusion in log
+    address oldPauseGuardian = pauseGuardian;
 
-        eqAssetGroup[groupId] = EqualAssets(
-            groupId,
-            groupName,
-            inGroupCTokenRateMantissa,
-            inGroupSuTokenRateMantissa,
-            interGroupCTokenRateMantissa,
-            interGroupSuTokenRateMantissa
-        );
-        equalAssetsGroupNum++;
-        return uint256(Error.NO_ERROR);
-    }
+    // Store pauseGuardian with value newPauseGuardian
+    pauseGuardian = newPauseGuardian;
 
-    function removeEqAssetGroup(uint8 groupId) public returns (uint256) {
-        // Check caller is admin
-        if (msg.sender != admin) {
-            return
-                fail(
-                    Error.UNAUTHORIZED,
-                    FailureInfo.SET_EQUAL_ASSET_GROUP_OWNER_CHECK
-                );
-        }
+    // Emit NewPauseGuardian(OldPauseGuardian, NewPauseGuardian)
+    emit NewPauseGuardian(oldPauseGuardian, pauseGuardian);
 
-        delete eqAssetGroup[groupId];
-        equalAssetsGroupNum--;
-        return uint256(Error.NO_ERROR);
-    }
+    return uint256(Error.NO_ERROR);
+  }
 
-    function getEqAssetGroup(uint8 groupId)
-        public
-        view
-        returns (EqualAssets memory)
-    {
-        return eqAssetGroup[groupId];
-    }
+  function _getPauseGuardian() public view returns (address) {
+    return pauseGuardian;
+  }
 
-    function getEqAssetGroupNum() public view returns (uint8) {
-        return equalAssetsGroupNum;
-    }
+  function _setMintPaused(CToken cToken, bool state) public returns (bool) {
+    //require(markets[address(cToken)].isListed, "cannot pause a market that is not listed");
+    require(msg.sender == pauseGuardian || msg.sender == admin, 'only pause guardian and admin can pause');
+    require(msg.sender == admin || state == true, 'only admin can unpause');
 
-    /**
-     * @notice Admin function to change the Pause Guardian
-     * @param newPauseGuardian The address of the new Pause Guardian
-     * @return uint 0=success, otherwise a failure. (See enum Error for details)
-     */
-    function _setPauseGuardian(address newPauseGuardian)
-        public
-        returns (uint256)
-    {
-        if (msg.sender != admin) {
-            return
-                fail(
-                    Error.UNAUTHORIZED,
-                    FailureInfo.SET_PAUSE_GUARDIAN_OWNER_CHECK
-                );
-        }
+    mintGuardianPaused[address(cToken)] = state;
+    emit ActionPaused(cToken, 'Mint', state);
+    return state;
+  }
 
-        // Save current value for inclusion in log
-        address oldPauseGuardian = pauseGuardian;
+  function _getMintPaused(CToken cToken) public returns (bool) {
+    return mintGuardianPaused[address(cToken)];
+  }
 
-        // Store pauseGuardian with value newPauseGuardian
-        pauseGuardian = newPauseGuardian;
+  function _setBorrowPaused(CToken cToken, bool state) public returns (bool) {
+    //require(markets[address(cToken)].isListed, "cannot pause a market that is not listed");
+    require(msg.sender == pauseGuardian || msg.sender == admin, 'only pause guardian and admin can pause');
+    require(msg.sender == admin || state == true, 'only admin can unpause');
 
-        // Emit NewPauseGuardian(OldPauseGuardian, NewPauseGuardian)
-        emit NewPauseGuardian(oldPauseGuardian, pauseGuardian);
+    borrowGuardianPaused[address(cToken)] = state;
+    emit ActionPaused(cToken, 'Borrow', state);
+    return state;
+  }
 
-        return uint256(Error.NO_ERROR);
-    }
+  function _getBorrowPaused(CToken cToken) public view returns (bool) {
+    return borrowGuardianPaused[address(cToken)];
+  }
 
-    function _getPauseGuardian() public view returns (address) {
-        return pauseGuardian;
-    }
+  function _setTransferPaused(bool state) public returns (bool) {
+    require(msg.sender == pauseGuardian || msg.sender == admin, 'only pause guardian and admin can pause');
+    require(msg.sender == admin || state == true, 'only admin can unpause');
 
-    function _setMintPaused(CToken cToken, bool state) public returns (bool) {
-        //require(markets[address(cToken)].isListed, "cannot pause a market that is not listed");
-        require(
-            msg.sender == pauseGuardian || msg.sender == admin,
-            "only pause guardian and admin can pause"
-        );
-        require(msg.sender == admin || state == true, "only admin can unpause");
+    transferGuardianPaused = state;
+    emit ActionPaused('Transfer', state);
+    return state;
+  }
 
-        mintGuardianPaused[address(cToken)] = state;
-        emit ActionPaused(cToken, "Mint", state);
-        return state;
-    }
+  function _getTransferPaused() public view returns (bool) {
+    return transferGuardianPaused;
+  }
 
-    function _getMintPaused(CToken cToken) public returns (bool) {
-        return mintGuardianPaused[address(cToken)];
-    }
+  function _setSeizePaused(bool state) public returns (bool) {
+    require(msg.sender == pauseGuardian || msg.sender == admin, 'only pause guardian and admin can pause');
+    require(msg.sender == admin || state == true, 'only admin can unpause');
 
-    function _setBorrowPaused(CToken cToken, bool state) public returns (bool) {
-        //require(markets[address(cToken)].isListed, "cannot pause a market that is not listed");
-        require(
-            msg.sender == pauseGuardian || msg.sender == admin,
-            "only pause guardian and admin can pause"
-        );
-        require(msg.sender == admin || state == true, "only admin can unpause");
+    seizeGuardianPaused = state;
+    emit ActionPaused('Seize', state);
+    return state;
+  }
 
-        borrowGuardianPaused[address(cToken)] = state;
-        emit ActionPaused(cToken, "Borrow", state);
-        return state;
-    }
+  function _getSeizePaused() public view returns (bool) {
+    return seizeGuardianPaused;
+  }
 
-    function _getBorrowPaused(CToken cToken) public view returns (bool) {
-        return borrowGuardianPaused[address(cToken)];
-    }
-
-    function _setTransferPaused(bool state) public returns (bool) {
-        require(
-            msg.sender == pauseGuardian || msg.sender == admin,
-            "only pause guardian and admin can pause"
-        );
-        require(msg.sender == admin || state == true, "only admin can unpause");
-
-        transferGuardianPaused = state;
-        emit ActionPaused("Transfer", state);
-        return state;
-    }
-
-    function _getTransferPaused() public view returns (bool) {
-        return transferGuardianPaused;
-    }
-
-    function _setSeizePaused(bool state) public returns (bool) {
-        require(
-            msg.sender == pauseGuardian || msg.sender == admin,
-            "only pause guardian and admin can pause"
-        );
-        require(msg.sender == admin || state == true, "only admin can unpause");
-
-        seizeGuardianPaused = state;
-        emit ActionPaused("Seize", state);
-        return state;
-    }
-
-    function _getSeizePaused() public view returns (bool) {
-        return seizeGuardianPaused;
-    }
-
-    /**
-     * @notice Return the address of the COMP token
-     * @return The address of COMP
-     */
-    function getCompAddress() public view returns (address) {
-        /*
+  /**
+   * @notice Return the address of the COMP token
+   * @return The address of COMP
+   */
+  function getCompAddress() public view returns (address) {
+    /*
         return 0xc00e94Cb662C3520282E6f5717214004A7f26888;
         */
-        return governanceToken;
+    return governanceToken;
+  }
+
+  /**
+   * @notice Return the address of the COMP token
+   * @param _governanceToken The address of COMP(governance token)
+   */
+  function setGovTokenAddress(address _governanceToken) public {
+    //require(adminOrInitializing(), "only admin can set governanceToken");
+    require(msg.sender == admin, 'only admin can set');
+    governanceToken = _governanceToken;
+  }
+
+  /**
+   * @notice Set the given borrow caps for the given cToken markets. Borrowing that brings total borrows to or above borrow cap will revert.
+   * @dev Admin or borrowCapGuardian function to set the borrow caps. A borrow cap of 0 corresponds to unlimited borrowing.
+   * @param cTokens The addresses of the markets (tokens) to change the borrow caps for
+   * @param newBorrowCaps The new borrow cap values in underlying to be set. A value of 0 corresponds to unlimited borrowing.
+   */
+  function _setMarketBorrowCaps(CToken[] calldata cTokens, uint256[] calldata newBorrowCaps) external {
+    require(
+      msg.sender == admin || msg.sender == borrowCapGuardian,
+      'only admin or borrow cap guardian can set borrow caps'
+    );
+
+    uint256 numMarkets = cTokens.length;
+    uint256 numBorrowCaps = newBorrowCaps.length;
+
+    require(numMarkets != 0 && numMarkets == numBorrowCaps, 'invalid input');
+
+    for (uint256 i = 0; i < numMarkets; i++) {
+      borrowCaps[address(cTokens[i])] = newBorrowCaps[i];
+      emit NewBorrowCap(cTokens[i], newBorrowCaps[i]);
     }
+  }
 
-    /**
-     * @notice Return the address of the COMP token
-     * @param _governanceToken The address of COMP(governance token)
-     */
-    function setGovTokenAddress(address _governanceToken) public {
-        //require(adminOrInitializing(), "only admin can set governanceToken");
-        require(msg.sender == admin, "only admin can set");
-        governanceToken = _governanceToken;
-    }
+  function _getMarketBorrowCap(CToken cToken) external view returns (uint256) {
+    return borrowCaps[address(cToken)];
+  }
 
-    /**
-     * @notice Set the given borrow caps for the given cToken markets. Borrowing that brings total borrows to or above borrow cap will revert.
-     * @dev Admin or borrowCapGuardian function to set the borrow caps. A borrow cap of 0 corresponds to unlimited borrowing.
-     * @param cTokens The addresses of the markets (tokens) to change the borrow caps for
-     * @param newBorrowCaps The new borrow cap values in underlying to be set. A value of 0 corresponds to unlimited borrowing.
-     */
-    function _setMarketBorrowCaps(
-        CToken[] calldata cTokens,
-        uint256[] calldata newBorrowCaps
-    ) external {
-        require(
-            msg.sender == admin || msg.sender == borrowCapGuardian,
-            "only admin or borrow cap guardian can set borrow caps"
-        );
+  /**
+   * @notice Admin function to change the Borrow Cap Guardian
+   * @param newBorrowCapGuardian The address of the new Borrow Cap Guardian
+   */
+  function _setBorrowCapGuardian(address newBorrowCapGuardian) external {
+    require(msg.sender == admin, 'only admin can set borrow cap guardian');
 
-        uint256 numMarkets = cTokens.length;
-        uint256 numBorrowCaps = newBorrowCaps.length;
+    // Save current value for inclusion in log
+    address oldBorrowCapGuardian = borrowCapGuardian;
 
-        require(
-            numMarkets != 0 && numMarkets == numBorrowCaps,
-            "invalid input"
-        );
+    // Store borrowCapGuardian with value newBorrowCapGuardian
+    borrowCapGuardian = newBorrowCapGuardian;
 
-        for (uint256 i = 0; i < numMarkets; i++) {
-            borrowCaps[address(cTokens[i])] = newBorrowCaps[i];
-            emit NewBorrowCap(cTokens[i], newBorrowCaps[i]);
-        }
-    }
+    // Emit NewBorrowCapGuardian(OldBorrowCapGuardian, NewBorrowCapGuardian)
+    emit NewBorrowCapGuardian(oldBorrowCapGuardian, newBorrowCapGuardian);
+  }
 
-    function _getMarketBorrowCap(CToken cToken)
-        external
-        view
-        returns (uint256)
-    {
-        return borrowCaps[address(cToken)];
-    }
+  function _getBorrowCapGuardian() external view returns (address) {
+    return borrowCapGuardian;
+  }
 
-    /**
-     * @notice Admin function to change the Borrow Cap Guardian
-     * @param newBorrowCapGuardian The address of the new Borrow Cap Guardian
-     */
-    function _setBorrowCapGuardian(address newBorrowCapGuardian) external {
-        require(msg.sender == admin, "only admin can set borrow cap guardian");
+  /**
+   * @notice Admin function to change the suTokenRateMantissa
+   * @param  _suTokenRateMantissa The address of the new suTokenRateMantissa
+   */
+  function _setSuTokenRateMantissa(uint256 _suTokenRateMantissa) external {
+    require(msg.sender == admin, 'only admin can set suTokenRateMantissa');
+    suTokenRateMantissa = _suTokenRateMantissa;
+  }
 
-        // Save current value for inclusion in log
-        address oldBorrowCapGuardian = borrowCapGuardian;
-
-        // Store borrowCapGuardian with value newBorrowCapGuardian
-        borrowCapGuardian = newBorrowCapGuardian;
-
-        // Emit NewBorrowCapGuardian(OldBorrowCapGuardian, NewBorrowCapGuardian)
-        emit NewBorrowCapGuardian(oldBorrowCapGuardian, newBorrowCapGuardian);
-    }
-
-    function _getBorrowCapGuardian() external view returns (address) {
-        return borrowCapGuardian;
-    }
-
-    /**
-     * @notice Admin function to change the suTokenRateMantissa
-     * @param  _suTokenRateMantissa The address of the new suTokenRateMantissa
-     */
-    function _setSuTokenRateMantissa(uint256 _suTokenRateMantissa) external {
-        require(msg.sender == admin, "only admin can set suTokenRateMantissa");
-        suTokenRateMantissa = _suTokenRateMantissa;
-    }
-
-    function _getSuTokenRateMantissa() external view returns (uint256) {
-        return suTokenRateMantissa;
-    }
+  function _getSuTokenRateMantissa() external view returns (uint256) {
+    return suTokenRateMantissa;
+  }
 }
--- contracts/UnderwriterStorage.sol
+++ contracts/UnderwriterStorage.sol
@@ -1,102 +1,93 @@
 pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
-import "./CToken.sol";
-import "./PriceOracle.sol";
+import './CToken.sol';
+import './PriceOracle.sol';
 
 contract UnderwriterProxyStorage {
-    /**
-     * @notice Administrator for this contract
-     */
-    address public admin;
+  /**
+   * @notice Administrator for this contract
+   */
+  address public admin;
 
-    /**
-     * @notice Pending administrator for this contract
-     */
-    address public pendingAdmin;
+  /**
+   * @notice Pending administrator for this contract
+   */
+  address public pendingAdmin;
 
-    /**
-     * @notice Active brains of Unitroller
-     */
-    address public implementation;
+  /**
+   * @notice Active brains of Unitroller
+   */
+  address public implementation;
 
-    /**
-     * @notice Pending brains of Unitroller
-     */
-    address public pendingImplementation;
+  /**
+   * @notice Pending brains of Unitroller
+   */
+  address public pendingImplementation;
+}
 
-    /**
-     * @notice Gov token address  Move to Admin
-     */
-    //address public governanceToken;
+contract UnderwriterStorage is UnderwriterProxyStorage {
+  address public admin;
+  address public governanceToken;
 
-    address public underWriterAdmin;
+  uint256 public suTokenRateMantissa;
+
+  /**
+   * @notice eqAssetGroup, cToken -> equal assets info.
+   */
+
+  uint8 public equalAssetsGroupNum;
+
+  /**
+   * @notice The Pause Guardian can pause certain actions as a safety mechanism.
+   *  Actions which allow users to remove their own assets cannot be paused.
+   *  Liquidation / seizing / transfer can only be paused globally, not by market.
+   */
+  address public pauseGuardian;
+  bool public _mintGuardianPaused;
+  bool public _borrowGuardianPaused;
+  bool public transferGuardianPaused;
+  bool public seizeGuardianPaused;
+  mapping(address => bool) public mintGuardianPaused;
+  mapping(address => bool) public borrowGuardianPaused;
+
+  // @notice The borrowCapGuardian can set borrowCaps to any number for any market. Lowering the borrow cap could disable borrowing on the given market.
+  address public borrowCapGuardian;
+
+  // @notice Borrow caps enforced by borrowAllowed for each cToken address. Defaults to zero which corresponds to unlimited borrowing.
+  mapping(address => uint256) public borrowCaps;
 }
 
 contract UnderwriterAdminInterface {
-    /// @notice EqualAssets, contains information of groupName and rateMantissas
-    struct EqualAssets {
-        uint8 groupId;
-        string groupName;
-        uint256 inGroupCTokenRateMantissa;
-        uint256 inGroupSuTokenRateMantissa;
-        uint256 interGroupCTokenRateMantissa;
-        uint256 interGroupSuTokenRateMantissa;
-    }
+  /// @notice EqualAssets, contains information of groupName and rateMantissas
+  struct EqualAssets {
+    uint8 groupId;
+    string groupName;
+    uint256 inGroupCTokenRateMantissa;
+    uint256 inGroupSuTokenRateMantissa;
+    uint256 interGroupCTokenRateMantissa;
+    uint256 interGroupSuTokenRateMantissa;
+  }
+  mapping(uint8 => EqualAssets) public eqAssetGroup;
 
-    function getEqAssetGroupNum() public view returns (uint8);
+  function getEqAssetGroupNum() public view returns (uint8);
 
-    function getEqAssetGroup(uint8 groupId)
-        public
-        view
-        returns (EqualAssets memory);
+  function getEqAssetGroup(uint8 groupId) public view returns (EqualAssets memory);
 
-    function _getPauseGuardian() public view returns (address);
+  function _getPauseGuardian() public view returns (address);
 
-    function _getMintPaused(CToken cToken) public returns (bool);
+  function _getMintPaused(CToken cToken) public returns (bool);
 
-    function _getTransferPaused() public view returns (bool);
+  function _getTransferPaused() public view returns (bool);
 
-    function _getBorrowPaused(CToken cToken) public view returns (bool);
+  function _getBorrowPaused(CToken cToken) public view returns (bool);
 
-    function _getSeizePaused() public view returns (bool);
+  function _getSeizePaused() public view returns (bool);
 
-    function getCompAddress() public view returns (address);
+  function getCompAddress() public view returns (address);
 
-    function _getMarketBorrowCap(CToken cToken) external view returns (uint256);
+  function _getMarketBorrowCap(CToken cToken) external view returns (uint256);
 
-    function _getBorrowCapGuardian() external view returns (address);
+  function _getBorrowCapGuardian() external view returns (address);
 
-    function _getSuTokenRateMantissa() external view returns (uint256);
-}
-
-contract UnderwriterStorage is UnderwriterAdminInterface {
-    address public admin;
-    address public governanceToken;
-
-    uint256 public suTokenRateMantissa;
-
-    /**
-     * @notice eqAssetGroup, cToken -> equal assets info.
-     */
-    mapping(uint8 => EqualAssets) public eqAssetGroup;
-    uint8 public equalAssetsGroupNum;
-
-    /**
-     * @notice The Pause Guardian can pause certain actions as a safety mechanism.
-     *  Actions which allow users to remove their own assets cannot be paused.
-     *  Liquidation / seizing / transfer can only be paused globally, not by market.
-     */
-    address public pauseGuardian;
-    bool public _mintGuardianPaused;
-    bool public _borrowGuardianPaused;
-    bool public transferGuardianPaused;
-    bool public seizeGuardianPaused;
-    mapping(address => bool) public mintGuardianPaused;
-    mapping(address => bool) public borrowGuardianPaused;
-
-    // @notice The borrowCapGuardian can set borrowCaps to any number for any market. Lowering the borrow cap could disable borrowing on the given market.
-    address public borrowCapGuardian;
-
-    // @notice Borrow caps enforced by borrowAllowed for each cToken address. Defaults to zero which corresponds to unlimited borrowing.
-    mapping(address => uint256) public borrowCaps;
+  function _getSuTokenRateMantissa() external view returns (uint256);
 }
