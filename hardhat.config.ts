650ba343e286caff92b54b7378ba91a170231ecd
Simon.Z <zhanghan.simon@gmail.com>
2023-04-12 11:39:42 -0700

    add  hardhat-contract-sizer for size calculation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,7 @@ import 'hardhat-contract-sizer';
 import 'hardhat-gas-reporter';
 import 'hardhat-tracer';
 import 'hardhat-etherscan-abi';
+import 'hardhat-contract-sizer';
 
 import './tasks';
 import { compileSetting } from './scripts/deployTool';
@@ -23,57 +24,57 @@ const argv = require('yargs/yargs')()
   .options({
     hardhatChainId: {
       type: 'number',
-      default: 31337,
+      default: 31337
     },
     ganacheRpc: {
       type: 'string',
-      default: 'http://127.0.0.1:7545',
+      default: 'http://127.0.0.1:7545'
     },
     maticRpcUrl: {
       type: 'string',
-      default: '',
+      default: ''
     },
     mumbaiRpcUrl: {
       type: 'string',
-      default: 'https://matic-mumbai.chainstacklabs.com',
+      default: 'https://matic-mumbai.chainstacklabs.com'
     },
     ethRpcUrl: {
       type: 'string',
-      default: '',
+      default: ''
     },
     ftmRpcUrl: {
       type: 'string',
-      default: '',
+      default: ''
     },
     meterTestRpcUrl: {
       type: 'string',
-      default: 'https://rpctest.meter.io',
+      default: 'https://rpctest.meter.io'
     },
     meterMainRpcUrl: {
       type: 'string',
-      default: 'https://rpc.meter.io',
+      default: 'https://rpc.meter.io'
     },
     baseTestRpcUrl: {
       type: 'string',
-      default: 'https://goerli.base.org',
+      default: 'https://goerli.base.org'
     },
     networkScanKey: {
       type: 'string',
-      default: '',
+      default: ''
     },
     privateKey: {
       type: 'string',
-      default: 'b55c9fcc2c60993e5c539f37ffd27d2058e7f77014823b461323db5eba817518', // random account
+      default: 'b55c9fcc2c60993e5c539f37ffd27d2058e7f77014823b461323db5eba817518' // random account
     },
     maticForkBlock: {
-      type: 'number',
+      type: 'number'
     },
     mumbaiForkBlock: {
-      type: 'number',
+      type: 'number'
     },
     ftmForkBlock: {
-      type: 'number',
-    },
+      type: 'number'
+    }
   }).argv;
 
 export default {
@@ -108,14 +109,14 @@ export default {
                   ? argv.mumbaiForkBlock !== 0
                     ? argv.mumbaiForkBlock
                     : undefined
-                  : undefined,
+                  : undefined
             }
           : undefined,
       accounts: {
         mnemonic: 'test test test test test test test test test test test junk',
         path: "m/44'/60'/0'/0",
-        accountsBalance: '100000000000000000000000000000',
-      },
+        accountsBalance: '100000000000000000000000000000'
+      }
     },
     matic: {
       url: argv.maticRpcUrl,
@@ -124,27 +125,27 @@ export default {
       // gas: 19_000_000,
       // gasPrice: 100_000_000_000,
       gasMultiplier: 1.3,
-      accounts: [argv.privateKey],
+      accounts: [argv.privateKey]
     },
     mumbai: {
       url: argv.mumbaiRpcUrl,
       chainId: 80001,
       timeout: 99999,
       // gasPrice: 100_000_000_000,
-      accounts: [argv.privateKey],
+      accounts: [argv.privateKey]
     },
     ftm: {
       url: argv.ftmRpcUrl,
       chainId: 250,
       timeout: 99999,
-      accounts: [argv.privateKey],
+      accounts: [argv.privateKey]
     },
     metertest: {
       url: argv.meterTestRpcUrl,
       chainId: 83,
       timeout: 99999,
       gasPrice: 500000000000,
-      accounts:{mnemonic:process.env.MNEMONIC_1}
+      accounts: { mnemonic: process.env.MNEMONIC_1 }
       // accounts: [
       //   process.env.PRIVATE_KEY_0,
       //   process.env.PRIVATE_KEY_1,
@@ -157,45 +158,45 @@ export default {
       chainId: 82,
       timeout: 99999,
       gasPrice: 500000000000,
-      accounts: [process.env.PRIVATE_KEY_0, process.env.PRIVATE_KEY_1],
+      accounts: [process.env.PRIVATE_KEY_0, process.env.PRIVATE_KEY_1]
     },
     basetest: {
       url: argv.baseTestRpcUrl,
       chainId: 84531,
-      accounts: [process.env.PRIVATE_KET_BASE],
+      accounts: [process.env.PRIVATE_KET_BASE]
     },
     ganache: {
       url: argv.ganacheRpc,
       chainId: 1337,
-      accounts: [process.env.PRIVATE_KEY_0, '0x34cd0e9ec7a0ec36a37db424f720de58223f9cd52e0175c7a972a6a864eb86a5'],
-    },
+      accounts: [process.env.PRIVATE_KEY_0, '0x34cd0e9ec7a0ec36a37db424f720de58223f9cd52e0175c7a972a6a864eb86a5']
+    }
   },
   etherscan: {
-    apiKey: argv.networkScanKey,
+    apiKey: argv.networkScanKey
   },
   solidity: {
-    compilers: [compileSetting('0.6.11', 200), compileSetting('0.7.6', 10), compileSetting('0.8.11', 200)],
+    compilers: [compileSetting('0.6.11', 200), compileSetting('0.7.6', 10), compileSetting('0.8.11', 200)]
   },
   paths: {
     sources: './contracts',
     tests: './test',
     cache: './cache',
-    artifacts: './artifacts',
+    artifacts: './artifacts'
   },
   mocha: {
-    timeout: 9999999999,
+    timeout: 9999999999
   },
   contractSizer: {
     alphaSort: false,
     runOnCompile: false,
-    disambiguatePaths: false,
+    disambiguatePaths: false
   },
   gasReporter: {
     enabled: false,
     currency: 'USD',
-    gasPrice: 21,
+    gasPrice: 21
   },
   typechain: {
-    outDir: 'typechain',
-  },
+    outDir: 'typechain'
+  }
 };
