f07138ddc34e95b2b5ee7198568ae856b4024fa9
崔棉大师 <1046166@qq.com>
2023-07-31 15:14:22 +0800

    deploy script

--- tasks/deploy/cToken/VeSumer.ts
+++ tasks/deploy/cToken/VeSumer.ts
@@ -16,9 +16,26 @@ task('dve', 'deploy veSumer contracts')
   .addOptionalParam('gasprice', 'gas price', 0, types.int)
   .setAction(async ({ sumer, rpc, pk, gasprice }, { ethers, run, network }) => {
     await run('compile');
-    await run('d', {
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+    const wallet = new ethers.Wallet(pk, provider);
+    const ve = await run('d', {
       name: 'VeSumer',
       args: [sumer],
+      rpc: rpc,
+      pk: pk,
+      gasprice: gasprice
+    });
+
+    const controller = await run('d', {
+      name: 'FraxGaugeController',
+      args: [sumer, ve.address],
+      rpc: rpc,
+      pk: pk,
+      gasprice: gasprice
+    });
+    const distributor = await run('d', {
+      name: 'FraxGaugeFXSRewardsDistributor',
+      args: [wallet.address, wallet.address, sumer, controller.address],
       rpc: rpc,
       pk: pk,
       gasprice: gasprice
