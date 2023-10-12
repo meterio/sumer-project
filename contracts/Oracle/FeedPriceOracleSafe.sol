9843745f4b7d20bb879990348cde9364c08cdc2b
崔棉大师 <1046166@qq.com>
2023-10-12 08:25:12 +0800

    update pyth oracle and cether template

--- contracts/Oracle/FeedPriceOracleSafe.sol
+++ contracts/Oracle/FeedPriceOracleSafe.sol
@@ -4,7 +4,7 @@ import './FeedPriceOracle.sol';
 
 contract FeedPriceOracleSafe is FeedPriceOracle {
   function _getPythPrice(FeedData memory feed) internal view override returns (uint256) {
-    PythStructs.Price memory price = IPyth(feed.addr).getPrice(feed.feedId);
+    PythStructs.Price memory price = IPyth(feed.addr).getPriceNoOlderThan(feed.feedId, 1800);
     uint256 decimals = DECIMALS - uint32(price.expo * -1);
     require(decimals <= DECIMALS, 'DECIMAL UNDERFLOW');
     return uint64(price.price) * (10 ** decimals);
