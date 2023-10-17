c2765cf025ce39642d722366776a52dfcf797b7a
崔棉大师 <1046166@qq.com>
2023-10-17 17:13:24 +0800

    update admin control

--- scripts/AdminControl.ts
+++ scripts/AdminControl.ts
@@ -1,9 +1,25 @@
 import { ethers } from 'hardhat';
-import { network_config, setNetwork, selectContract, sendTransaction } from './helper';
+import {
+  network_config,
+  setNetwork,
+  selectContract,
+  sendTransaction,
+  green,
+  DEFAULT_ADMIN_ROLE,
+  MINTER_ROLE
+} from './helper';
 import { input, select } from '@inquirer/prompts';
 import { utils } from 'ethers';
 
 async function main() {
+  const coder = new utils.AbiCoder();
+  console.log('DEFAULT_ADMIN_ROLE', green(DEFAULT_ADMIN_ROLE.toString()));
+  console.log('MINTER_ROLE', green(MINTER_ROLE.toString()));
+  console.log('PAUSER_ROLE', green('0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a'));
+  console.log('CAPPER_ROLE', green('0x585dbb3f830c4325b9ffb1ad42c16ada76c518747b0fb7ee297b261fc6a0f29f'));
+  console.log('EMERGENCY_ADMIN', green('0x5c91514091af31f62f596a314af7d5be40146b2f2355969392f055e12e0982fb'));
+  console.log('COMP_LOGIC', green('0x750fc7a006f78e0009a09aa4bf97546d802b8f6c9d27e623dd7cc78951321eaf'));
+
   const network = await setNetwork(network_config);
   let { wallet, override } = network;
 
